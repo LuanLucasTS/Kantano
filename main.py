f00c3ae1ced5d96a025f0194b074d2743f44e53f
Luan <luanlucascopr@gmail.com>
2023-02-06 01:18:48 -0400

    criando os controles pra um video

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Kantano'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return render_template('video.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
